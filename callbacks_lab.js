e1d74753beba138ca49d34dfcb62bb61bc13fdaf
maria liza <mariamartinez@pursuit.org>
2019-10-16 20:20:04 -0400

    repush to finish

--- callbacks_lab.js
+++ callbacks_lab.js
@@ -23,10 +23,38 @@
 // }
 // console.log(truOrNot(1));
 
-const string = (str) => {
-    return "hi"
+// const string = (str) => {
+//     return "hi"
+// }
+// const thatManyTimes = (num) => {
+//     return string() += num
+// }
+// console.log(thatManyTimes(3))
+// const myForEach = (arr, callback) => {
+//   for (let i = 0; i < arr.length; i++) {
+//    callback(arr[i]);
+// }
+
+// const newFunction = (arr) => {
+// let sum = 0
+// myForEach(arr, (el) => sum += el)
+
+// }
+// return sum
+// }
+
+// console.log(newFunction([1,2,3]))
+const myMap = (arr, cb) => {
+    let result = [];
+    for (let i=0; i<arr.length; i++) {
+        result.push(cb(arr[i]));
+    }
+    return result
+    }
+
+const zeroFunction = (arr) => {
+    return myMap(arr, (el) => {
+        return 0;
+    })
 }
-const thatManyTimes = (num) => {
-    return string() += num
-}
-console.log(thatManyTimes(3))
+console.log(zeroFunction([1,2,3]));
